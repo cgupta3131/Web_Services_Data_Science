d034c55b429fe5061e62b4f7ded7274aec7b71e2
LavishGulati <lavish00gulati@gmail.com>
2019-04-27 16:01:12 +0530

    Initialized project

---
+++ home/index.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<html>
+<head>
+
+</head>
+
+<body>
+
+    <div id="carouselExampleControls" class="carousel slide embed-responsive embed-responsive-16by9" data-ride="carousel">
+        <div class="carousel-inner embed-responsive-item">
+            <div class="carousel-item active">
+                <img src="{{pic}}" class="d-block w-100" alt="image">
+            </div>
+            <div class="carousel-item">
+                <img src="{{pic}}" class="d-block w-100" alt="image">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+</body>
+</html>
